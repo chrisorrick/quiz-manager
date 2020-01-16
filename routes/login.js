2806f00632bb7f9785a04d418e34c657fa93d140
Christopher Orrick <christopherorrick@me.com>
2020-01-16 14:27:44 +0000

    Create admin user route for set up

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const passport = require("passport");
+const User = require("../models/User");
 const router = express.Router();
 
 router.get('/', async function (req, res) {
@@ -19,6 +20,15 @@ router.post('/', passport.authenticate('local', {
 
 router.get('/logout', async function (req, res) {
   req.logout();
+  res.redirect('/login');
+});
+
+router.get('/admin', function (req, res) {
+  const adminJSON = new User(require("../models/admin-user.json"));
+  adminJSON.save(function (err, user) {
+    if (err) return console.error(err);
+  });
+
   res.redirect('/login');
 });
 
